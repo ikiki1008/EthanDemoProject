fc43db949ba6cf766a9a9140ba72c701528ed3c4
Yul <miayu.1008@gmail.com>
2025-05-10 11:20:22 +0900

    fragment issue fixed

---
+++ app/src/main/java/com/example/myapplication/ui/theme/fivetaps/MyPageFragment.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication.ui.theme.fivetaps
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+class MyPageFragment : Fragment() {
+    override fun onCreateView(
+
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = ComposeView(requireContext())
+        view.apply {
+            setContent {
+                Log.d("Activity", "this is my page screen")
+                Text("this is a text box55555")
+            }
+        }
+        return view
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/ui/theme/fivetaps/ShoppingFragment.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication.ui.theme.fivetaps
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+class ShoppingFragment : Fragment() {
+    override fun onCreateView(
+
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = ComposeView(requireContext())
+        view.apply {
+            setContent {
+                Log.d("Activity", "this is shopping screen")
+                Text("this is a text 3333")
+            }
+        }
+        return view
+    }
+
+}
